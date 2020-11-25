0c3eb8ba2669f3e6e01f31b763c4067a7bb4b7b9
velmurugan ramadasan <vramadas@sg.ibm.com>
2020-11-25 22:11:18 +0800

    Add req ip for debug

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res) {
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render('index.html', { pageCountMessage : req.ip});
   }
 });
 
